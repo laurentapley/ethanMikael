c8b1e7e581191de30102a7bd8e1e0673056bb679
Lauren Tapley <laurentapley@Laurens-MacBook-Pro.local>
2017-07-26 23:19:20 -0700

    styling additions

--- headshots.php
+++ headshots.php
@@ -3,12 +3,19 @@ require("inc/req_globals.php");
 include("inc/include_head.php");
 include("inc/include_nav.php");
  ?>
-    <div class="headshots">
-        <img src="img/" alt="">
-        <img src="img/" alt="">
-        <img src="img/" alt="">
-        <img src="img/" alt="">
+    <div class="headshot-container">
+        <div class="slidebox">
+            <img src="_img/EthanBizCom.jpg" alt="Ethan Mikael Headshot">
+        </div>
+        <div class="headshots">
+            <img src="_img/EthanBizCom.jpg" alt="Ethan Mikael Headshot">
+            <img src="_img/Ethan.jpg" alt="Ethan Mikael Headshot">
+            <img src="_img/EthanBusiness.jpg" alt="Ethan Mikael Headshot">
+            <img src="_img/EthanDruggie.jpg" alt="Ethan Mikael Headshot">
+            <img src="_img/EthanSurfer.jpg" alt="Ethan Mikael Headshot">
+        </div>
     </div>
+
 
  <?php
  include("inc/include_footer.php");
--- inc/include_footer.php
+++ inc/include_footer.php
@@ -1,16 +1,15 @@
-<footer>
-    <div class="bottom-bar">
-        <p>Ethan Mikael &copy; All rights reserved.</p><br>
-        <!-- <p>Web Service by Whet Web</p> -->
-    </div>
-    <div class="social">
-        <a href="#" class="fa fa-twitter"></a>
-        <a href="#" class="fa fa-instagram"></a>
-        <a href="#" class="fa fa-facebook"></a>
-        <a href="#" class="fa fa-imdb"></a>
-    </div>
-</footer>
-</main>
-
-</body>
+    </main>
+        <footer>
+            <div class="bottom-bar">
+                <p>Ethan Mikael &copy; All rights reserved.</p><br>
+                <!-- <p>Web Service by Whet Web</p> -->
+            </div>
+            <div class="social">
+                <a href="#" class="fa fa-twitter"></a>
+                <a href="#" class="fa fa-instagram"></a>
+                <a href="#" class="fa fa-facebook"></a>
+                <a href="#" class="fa fa-imdb"></a>
+            </div>
+        </footer>
+    </body>
 </html>
